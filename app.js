a837c0f75fcfacb643c155228acf03e436666538
Lucas Velazquez <37420750+LucasVelazquez@users.noreply.github.com>
2019-11-02 14:58:05 -0300

    Add app.js
    Initial commit

---
+++ app.js
@@ -0,0 +1,1 @@
+javascript:(function(){var loginPopup = document.getElementsByClassName('RnEpo')[0];if(typeof loginPopup != 'undefined') {loginPopup.remove();document.body.style.overflow = '';}})();
